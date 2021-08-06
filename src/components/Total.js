adbfb40e97982334704ea5620eb89b66979ab76f
harloxx <chloe0105@naver.com>
2021-08-06 18:11:36 +0900

    add total

--- src/components/Total.js
+++ src/components/Total.js
@@ -8,10 +8,14 @@ import { Bar, Doughnut, Line} from "react-chartjs-2"
 
 const Total=()=>{
     const [comparedData,setComparedData]=useState({})
+    const [todayData,setTodayData]=useState(null)
+
     useEffect(()=>{
+      setTodayData(null);
         const fetchEvents=async()=>{
           const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
           makeData(res.data)
+          setTodayData(res.data)
         }
     
         const makeData=(items)=>{
@@ -60,6 +64,56 @@ const Total=()=>{
         fetchEvents();
     
       },[])
+
+      let today = new Date();   
+
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate()-1;  // 날짜
+    if (date<=2){
+      if (month==1){
+          year=year-1;
+          month=12
+          if(date==1) date=30
+          else date=31
+      }
+      else if (month==5||month==7||month==9||month==11){
+          month=month-1
+          if(date==1) date=30
+          else date=31
+      }
+      else if(month==3){
+          month=month-1
+          if(date==1) date=27
+          else date=28
+      }
+      else{
+          month=month-1
+          if(date==1) date=30
+          else date=31
+      }
+  }    
+
+  if (month<10){
+      month=String(month)
+      month='0'+month
+  }
+  if (date<10){
+      date=String(date)
+      date='0'+date
+  }
+  const firstFinal=String(year)+'-'+String(month)+'-'+'01T00:00:00Z'
+  const final=String(year)+'-'+String(month)+'-'+String(date)+'T00:00:00Z'
+  
+  
+
+    const nowMonth=year+'년 '+month+'월 '+date+'일 '+'국내 누적 확진자 현황 입니다'
+  const monthConfirmed=month+'월 1일 기준 누적 확진자 수는 '
+  const monthRecovered=month+'월 1일 기준 누적 격리해제자 수는 '
+  const monthDeath=month+'월 1일 기준 누적 사망자 수는 '
+  const nowNum=month+'월 '+date+'일, 금일 날짜 기준 격리자 수는 '
+    if (!todayData) return null;
+    
     
         return (
             <section>
@@ -71,6 +125,27 @@ const Total=()=>{
                 } />
               </div>
             </div>
+            {todayData.map(datee=>(
+                (datee.Date==firstFinal) ? (
+                    <div key={datee.Date}>
+                        <p>{monthConfirmed}{datee.Confirmed}명 입니다.</p>
+                    </div>)
+                :null        
+            ))}
+            {todayData.map(datee=>(
+                (datee.Date==firstFinal) ? (
+                    <div key={datee.Date}>
+                        <p>{monthRecovered}{datee.Recovered}명 입니다.</p>
+                    </div>)
+                :null        
+            ))}
+            {todayData.map(datee=>(
+                (datee.Date==firstFinal) ? (
+                    <div key={datee.Date}>
+                        <p>{monthDeath}{datee.Deaths}명 입니다.</p>
+                    </div>)
+                :null        
+            ))}
     
           </section>
         )
